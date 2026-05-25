feb1153708117304a751ec9590a3ac875400ac44
Baniwa <giulia.unb@gmail.com>
2026-05-25 15:07:54 -0300

    chore(project): add Spring Boot application entry point

---
+++ src/main/java/com/safepay/schedule/SafePayScheduleApplication.java
@@ -0,0 +1,12 @@
+package com.safepay.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePayScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafePayScheduleApplication.class, args);
+    }
+}
